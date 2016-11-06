bceebe080671dc553cb19358cf237717374e2654
Michael <master8.michael@yahoo.com>
2016-11-06 19:34:55 +0500

    add pokemon directive and controller

--- js/app.js
+++ js/app.js
@@ -14,29 +14,92 @@ app.config(function($routeProvider) {
 })
 .controller("pagesController",function($scope, $http, $interval){
 
-	$scope.x = 100;
-	$scope.y = 70;
 
-	$http.get("?controller=user").success(function (data) {
-		$scope.users = data;
-	});
+})
+.controller("gameController", function ($scope) {
 
-	var animation, time = 0;
+	$scope.level = 0;
+	$scope.health = 100;
+	$scope.score = 0;
 
-	var startAnimation = function () {
-		animation = $interval(function () {
-			time++;
-			$scope.x += 7*Math.sin(time/50);
-			$scope.y += 5*Math.cos(time/20);
-		}, 50);
-	};
-
-	startAnimation();
 })
 .controller("menuController", function ($scope, $http) {
 	$http.get("?controller=menu").success(function (data) {
 		$scope.items = data;
 	});
+})
+.controller("pokemonController", function ($http, $scope, $interval, $timeout) {
+
+	var pokemons;
+
+	$http.get("?controller=pokemon").success(function (data) {
+		pokemons = data;
+		$scope.next();
+	});
+
+	var current = 0;
+	$scope.power = 15;
+	$scope.speed = 4;
+	$scope.image = "images/pokemons/1.png";
+	$scope.active = "hidden";
+
+	var timer;
+	$scope.x = 0;
+	$scope.y = 20;
+
+	var animation, time = 0;
+
+	$scope.move = function () {
+		$scope.active = "visible";
+		animation = $interval(function () {
+			time++;
+			$scope.x += 5*Math.sin(time/50);
+			$scope.y += 2*Math.cos(time/20);
+		}, 50);
+
+		timer = $timeout(function () {
+			$scope.notCaught();
+		}, 7000)
+	};
+
+	$scope.notCaught = function () {
+		$rootScope.$emit("notCaughtPokemon");
+		$scope.stop();
+	};
+
+	$scope.caught = function () {
+		$rootScope.$emit("caughtPokemon");
+		$scope.stop();
+	};
+
+	$scope.stop = function () {
+		$scope.active = "hidden";
+		$interval.cancel(animation);
+		$timeout.cancel(timer);
+		time = 0;
+		$scope.x = 0;
+		$scope.y = 20;
+
+		$scope.next();
+	};
+
+	$scope.next = function () {
+
+		if (current < pokemons.length)
+		{
+			$scope.power = pokemons[current].power;
+			$scope.speed = pokemons[current].speed;
+			$scope.image = pokemons[current].image;
+
+			current++;
+
+			$scope.move();
+		}
+		else
+		{
+			$rootScope.$emit("endPokemons");
+		}
+	};
 })
 .directive("header", function(){
 	return {
@@ -44,7 +107,17 @@ app.config(function($routeProvider) {
 		replace: true,
 		restrict: 'E',
 		scope:{
-			current: '='
-		}
+		},
+		controller: "menuController"
+	}
+})
+.directive("pokemon", function () {
+	return {
+		templateUrl:"assets/directives/pokemon.html",
+		replace: true,
+		restrict: 'E',
+		scope:{
+		},
+		controller: "pokemonController"
 	}
 })
